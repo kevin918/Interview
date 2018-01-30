b252c4795293b581934fdf9fcce6cb0f9557a452
renkai <renkai@bilibili.com>
2018-01-30 13:55:56 +0800

    inorder traversal

--- src/leetcode/inorder.java
+++ src/leetcode/inorder.java
@@ -10,8 +10,21 @@ import java.util.Stack;
  */
 public class inorder {
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(2);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        List<Integer> res = in(root);
+        List<Integer> rec = recursive(root);
+        System.out.println("iterative: " + res + ", recursive: " + rec);
     }
+
+    /**
+     * iterative solution
+     * @param root
+     * @return
+     */
     static List<Integer> in(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> s = new Stack<>();
@@ -27,5 +40,26 @@ public class inorder {
             }
         }
         return res;
+    }
+
+
+    /**
+     * recursive solution
+     * @param root
+     * @return
+     */
+    static List<Integer> recursive(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(res, root);
+        return res;
+    }
+    static void helper(List<Integer> res, TreeNode node){
+        if(node.left != null) {
+            helper(res, node.left);
+        }
+        res.add(node.value);
+        if(node.right != null) {
+            helper(res, node.right);
+        }
     }
 }
