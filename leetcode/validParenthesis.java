d7ea00d3d841e2115d6df0666dc02e0ffc57dc34
renkai <renkai@bilibili.com>
2018-03-13 00:28:20 +0800

    lc20:valid parenthesis

--- leetcode/validParenthesis.java
+++ leetcode/validParenthesis.java
@@ -1,8 +1,23 @@
 import java.util.Stack;
 
 public class validParenthesis {
-    static void valid() {
+    static boolean valid(String input) {
         Stack s = new Stack();
-
+        for (char c : input.toCharArray()) {
+            if(c=='(') {
+                s.push(')');
+            } else if (c == '{') {
+                s.push('}');
+            } else if (c == '[') {
+                s.push(']');
+            } else if (s.isEmpty() || !s.pop().equals(c)) {
+                return false;
+            }
+        }
+        return s.isEmpty();
+    }
+    public static void main(String[] str) {
+        String test = "[[{{}}]]";
+        System.out.println("result: " + valid(test));
     }
 }
